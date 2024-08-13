6dd86c49735bae722d512de1e955bdbd64421d4c
codenamehenny <genesis09m@hotmail.com>
2024-08-13 18:12:05 -0400

    tally function added

--- ProductReviewAnalysis.py
+++ ProductReviewAnalysis.py
@@ -18,7 +18,7 @@ def uppercase_feedback(reviews, feedback):
     uppercase_review = []
     for review in reviews:
         for keyword in feedback:
-            if keyword.capitalize() in review:
+            if keyword.capitalize() in review: #selects the capitalized keyword and makes it uppercase
                 review = review.replace(keyword.capitalize(), keyword.upper())
             review = review.replace(keyword, keyword.upper())
         uppercase_review.append(review)
@@ -30,3 +30,27 @@ uppercase_review = uppercase_feedback(reviews, feedback)
 #displays updated reviews with the capitallized keywords
 for review in uppercase_review:
     print(review)
+
+# Task 2 Sentiment Tally
+# Develop a function that tallies the number of positive and negative words in each review.  
+# The function should return the total count of positive and negative words.
+
+positive_words = ["good", "excellent", "great", "awesome", "fantastic", "superb", "amazing"]
+negative_words = ["bad", "poor", "terrible", "horrible", "awful", "disappointing", "subpar"]
+
+# function that counts positive and negative words used in review list
+def finding_review_tally(reviews, positive_words, negative_words):
+    positive_count = 0
+    negative_count = 0
+    for review in reviews:
+        for positive in positive_words: # loops through positive words list, checking both uppercase and capitalized words
+            if positive.capitalize() or positive.upper() in review:
+                positive_count += review.count(positive) 
+        for negative in negative_words: # loops through negative list same way as the postive
+            if negative.capitalize() or negative.upper() in review:
+                negative_count += review.count(negative)
+            negative_count += review.count(negative)
+    return print(f"\nPositive word count: {positive_count} \nNegative word count: {negative_count} ")
+
+# calls tally function
+finding_review_tally(reviews, positive_words, negative_words)
